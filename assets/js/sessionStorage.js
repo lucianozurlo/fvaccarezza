ecc99089c21323ddd0741aebf35eb3de2ffa9938
Luciano Zurlo <lucianozurlo@gmail.com>
2026-02-03 11:16:42 -0300

    all

--- assets/js/sessionStorage.js
+++
@@ -1,8 +0,0 @@
-(function () {
-  const mode = sessionStorage.getItem("vt_mode");
-  const dir = sessionStorage.getItem("vt_dir");
-  if (mode) document.documentElement.dataset.vtMode = mode;
-  if (dir) document.documentElement.dataset.vtDir = dir;
-  sessionStorage.removeItem("vt_mode");
-  sessionStorage.removeItem("vt_dir");
-})();
